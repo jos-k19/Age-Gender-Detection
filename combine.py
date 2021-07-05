e26c430b97c37856c15be184437fee6819d54a1d
Jos <joshkatiyare1903@gmai.com>
2021-07-05 22:08:11 +0530

    commmit_06

--- combine.py
+++ combine.py
@@ -3,71 +3,7 @@ import time
 import argparse
 import os
 
-<<<<<<< Updated upstream
 
-
-def gender_age(frame, faceNet, ageNet, genderNet, minConf=0.5):
-
-    # define the list of age buckets our age detector will predict
-    AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)",
-                   "(38-43)", "(48-53)", "(60-100)"]
-    GENDER_BUCKET = ['Male', 'Female']
-
-    # initialize our results list
-    results = []
-    # grab the dimensions of the frame and then construct a blob from it
-    (h, w) = frame.shape[:2]
-    blob = cv.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
-    # pass the blob through the network and obtain the face detections
-    faceNet.setInput(blob)
-    detections = faceNet.forward()
-
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > minConf:
-            # compute the (x, y)-coordinates of the bounding box for he object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            # extract the ROI of the face
-            face = frame[startY:endY, startX:endX]
-            # ensure the face ROI is sufficiently large
-            if face.shape[0] < 20 or face.shape[1] < 20:
-                continue
-            # construct a blob from *just* the face ROI
-            faceBlob = cv.dnn.blobFromImage(face, 1.0, (227, 227),
-                                             (78.4263377603, 87.7689143744, 114.895847746),
-                                             swapRB=False)
-            # make predictions on the age and find the age bucket with
-            # the largest corresponding probability
-            ageNet.setInput(faceBlob)
-            genderNet.setInput(faceBlob)
-            agePreds = ageNet.forward()
-            genderPreds = genderNet.forward()
-
-            i = agePreds[0].argmax()
-            j = genderPreds[0].argmax()
-            age = AGE_BUCKETS[i]
-            gender = GENDER_BUCKET[j]
-            ageConfidence = agePreds[0][i]
-            genderConfidence = genderPreds[0][j]
-            # construct a dictionary consisting of both the face bounding box location along with the age prediction, then update our results list
-            d = {
-                "loc": (startX, startY, endX, endY),
-                "age": (age, ageConfidence),
-                "gender": (gender, genderConfidence)
-            }
-            results.append(d)
-    # return our results to the calling function
-    return results
-
-
-=======
->>>>>>> Stashed changes
 def getFaceBox(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
     frameHeight = frameOpencvDnn.shape[0]
@@ -88,12 +24,13 @@ def getFaceBox(net, frame, conf_threshold=0.7):
             cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 1)
     return frameOpencvDnn, bboxes
 
-# Code starts from here
-
 
 parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
-parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
+parser.add_argument('--input',
+                    help='Path to input image or video file. Skip this argument to capture frames from a camera.')
 parser.add_argument("--device", default="cpu", help="Device to inference on")
+
+args = parser.parse_args()
 
 args = parser.parse_args()
 
@@ -135,94 +72,9 @@ elif args.device == "gpu":
     print("Using GPU device")
 
 # Open a video file or an image file or a camera stream
-v = args.input
-print(f"v:{v}")
-
-<<<<<<< Updated upstream
-if v!="video":
-    cap = cv.VideoCapture(v)
-    padding = 20
-    while cv.waitKey(1) < 0:
-        # Read frame
-        t = time.time()
-        hasFrame, frame = cap.read()
-        if not hasFrame:
-            cv.waitKey()
-            break
-
-        frameFace, bboxes = getFaceBox(faceNet, frame)
-        if not bboxes:
-            print("No face Detected, Checking next frame")
-            continue
-
-        for bbox in bboxes:
-            # print(bbox)
-            face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
-                   max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
-
-            blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-            genderNet.setInput(blob)
-            genderPreds = genderNet.forward()
-            gender = genderList[genderPreds[0].argmax()]
-            print("Gender Output : {}".format(genderPreds))
-            print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
-
-            ageNet.setInput(blob)
-            agePreds = ageNet.forward()
-            age = ageList[agePreds[0].argmax()]
-            print("Age Output : {}".format(agePreds))
-            print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
-
-            label = "{},{}".format(gender, age)
-            cv.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2,
-                       cv.LINE_AA)
-            cv.imshow("Age Gender Demo", frameFace)
-            # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
-        print("time : {:.3f}".format(time.time() - t))
-
-
-
-
-else:
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
-    padding = 20
-    while True:
-        # grab the frame from the threaded video stream and resize it to have a maximum width of 400 pixels
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-        # detect faces in the frame, and for each face in the frame, predict the age
-        results = gender_age(frame, faceNet, ageNet, genderNet)
-        # loop over the results
-        for r in results:
-
-            (startX, startY, endX, endY) = r["loc"]
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-
-
-            print("Gender Output : {}".format(r["gender"][1] * 100))
-            print("Gender : {}, conf = {:.3f}".format(r["gender"][0], r["gender"][1] * 100))
-
-
-            print("Age Output : {}".format(r["age"][1] * 100))
-            print("Age : {}, conf = {:.3f}".format(r["age"][0], r["age"][1] * 100))
-
-            label = "{},{}".format(r["gender"][0],r["age"][0],)
-
-            cv.rectangle(frame, (startX, startY), (endX, endY), (0, 255,0), 2)
-            cv.putText(frame, label, (startX, y), cv.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 255), 2,cv.LINE_AA)
-
-        # show the output frame
-        cv.imshow("Frame", frame)
-        key = cv.waitKey(1) & 0xFF
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
-    # do a bit of cleanup
-    cv.destroyAllWindows()
-    vs.stop()
-=======
+v = args.input if args.input else 0
 cap = cv.VideoCapture(v)
+print(f"v:{v}")
 padding = 20
 while cv.waitKey(1) < 0:
     # Read frame
@@ -233,6 +85,7 @@ while cv.waitKey(1) < 0:
         break
 
     frameFace, bboxes = getFaceBox(faceNet, frame)
+    print(f'bboxes:{bboxes}')
     if not bboxes:
         print("No face Detected, Checking next frame")
         continue
@@ -261,8 +114,3 @@ while cv.waitKey(1) < 0:
         cv.imshow("Age Gender Demo", frameFace)
         # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
     print("time : {:.3f}".format(time.time() - t))
-
-
-
-
->>>>>>> Stashed changes
