17fc8203cfa3472717b91ab782f2111fbf0a3bbb
Jos <joshkatiyare1903@gmai.com>
2021-07-06 00:54:23 +0530

    commit_07

--- combine.py
+++ combine.py
@@ -7,31 +7,39 @@ def Face_det(net, frame, conf_threshold=0.7):
     frame_copy = frame.copy()
     h = frame_copy.shape[0]
     w = frame_copy.shape[1]
-    blob = cv.dnn.blobFromImage(frame_copy, 1.0, (300, 300), [104, 117, 123], True, False)
+    # resizes and crops image from center, subtract mean values, scales values by scalefactor, swap Blue and Red channels.
+    # returns a blob  after mean subtraction, normalizing, and channel swapping on our image
+    blob = cv.dnn.blobFromImage(frame_copy, 1.0, (300, 300), MODEL_MEAN_VALUES, True, False)
 
+    # function of the Inflater class is used to set input data for uncompression.
     net.setInput(blob)
+    # will give Numpy ndarray as output which you can use it to plot box on the given input image
     detections = net.forward()
     bboxes = []
+    # loop through shape - shape[2]
+    # k = 0
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
+        # create the bounding boxes
         if confidence > conf_threshold:
             x1 = int(detections[0, 0, i, 3] * w)
             y1 = int(detections[0, 0, i, 4] * h)
             x2 = int(detections[0, 0, i, 5] * w)
             y2 = int(detections[0, 0, i, 6] * h)
             bboxes.append([x1, y1, x2, y2])
-            cv.rectangle(frame_copy, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight / 150)), 1)
+            cv.rectangle(frame_copy, (x1, y1), (x2, y2), (0, 255, 0), int(round(h / 150)), 1)
+        # print(k)
+        # k+= 1
     return frame_copy, bboxes
 
+# code starts
 
 parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
-parser.add_argument('--input',
-                    help='Path to input image or video file. Skip this argument to capture frames from a camera.')
-parser.add_argument("--device", default="cpu", help="Device to inference on")
+parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
 
 args = parser.parse_args()
 
-args = parser.parse_args()
+# save our files
 
 faceProto = os.path.sep.join(["face_detector", "opencv_face_detector.pbtxt"])
 faceModel = os.path.sep.join(["face_detector","opencv_face_detector_uint8.pb"])
@@ -46,32 +54,16 @@ MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
 
-# Load network
+# Load our model
 ageNet = cv.dnn.readNet(ageModel, ageProto)
 genderNet = cv.dnn.readNet(genderModel, genderProto)
 faceNet = cv.dnn.readNet(faceModel, faceProto)
 
-if args.device == "cpu":
-    ageNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
-
-    genderNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
-
-    faceNet.setPreferableBackend(cv.dnn.DNN_TARGET_CPU)
-
-    print("Using CPU device")
-elif args.device == "gpu":
-    ageNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    ageNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
-
-    genderNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
-
-    genderNet.setPreferableBackend(cv.dnn.DNN_BACKEND_CUDA)
-    genderNet.setPreferableTarget(cv.dnn.DNN_TARGET_CUDA)
-    print("Using GPU device")
 
 # Open a video file or an image file or a camera stream
 v = args.input if args.input else 0
+# object to capture a video/image
+# for video v = 0
 cap = cv.VideoCapture(v)
 print(f"v:{v}")
 padding = 20
@@ -79,6 +71,7 @@ while cv.waitKey(1) < 0:
     # Read frame
     hasFrame, frame = cap.read()
     if not hasFrame:
+        # wait until window is not closed
         cv.waitKey()
         break
 
@@ -90,6 +83,7 @@ while cv.waitKey(1) < 0:
 
     for bbox in bboxes:
         # print(bbox)
+        # extract face from bounding boxes using x1, x2, y1 and y2
         face = frame[max(0, bbox[1] - padding):min(bbox[3] + padding, frame.shape[0] - 1),
                max(0, bbox[0] - padding):min(bbox[2] + padding, frame.shape[1] - 1)]
 
